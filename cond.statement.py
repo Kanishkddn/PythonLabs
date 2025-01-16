a9f0cb776931a89da90b88a18f1b9674f452ef30
kanishkddn <kanishkkumarddn@gmail.com>
2025-01-16 10:36:45 +0530

    LAB 5

---
+++ cond.statement.py
@@ -0,0 +1,42 @@
+# x=10
+# if(x<=10):
+#     print("Hello")
+#     x=x+1
+# print("Done")
+
+# temp=float(input("Enter the temp: "))
+# if(temp>30.0):
+#     print("It is Hot Outside!!!")
+#     print("Drink Plenty of water")
+
+# elif(temp>20.0):
+#     print("Temperatuure is ok outside")
+
+# elif(temp>10.0): #if we put temp as 21, it executes the 20+ elif and doesn't execute the elif blocks below the one executed
+#     print("Cold Outside")
+
+# else:
+#     print("Sit at Home")
+
+'''Write a python code to convert weight from kg to lbs and vice versa'''
+
+# xe=int(input("Enter the unit of your weight (1 for kg/2 for lbs): "))
+
+# if xe==1:
+#     wei=float(input("Enter your weight: "))
+#     fw=wei*2.20462
+#     print("Your weight in pounds is: ", fw)
+
+# elif xe==2:
+#     wei=float(input("Enter your weight: "))
+#     fw=wei*0.453592
+#     print("Your weight in pounds is: ", fw)
+# else:
+#     print("invalid Option")
+
+'''Make a Guessing Game'''
+print("This is a guessing Game")
+print("The number i have picked is between 1 and 50")
+print("You have 3 guesses")
+n=input("Take a guess")
+
